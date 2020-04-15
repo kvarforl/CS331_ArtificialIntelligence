099b1232808aa77c6cdc8a6dedf67889924ef999
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-04-15 12:35:24 -0700

    finished expand function

--- program1/p1.py
+++ program1/p1.py
@@ -2,7 +2,9 @@
 
 import argparse
 import ast
+from collections import defaultdict
 from queue import LifoQueue, Queue
+from itertools import combinations
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -22,9 +24,75 @@ def read_from_file(fname:str):
         right = "("+fp.readline()+")"
     return ast.literal_eval(left), ast.literal_eval(right)
 
+def state_is_legal(state):
+    left, right = state
+    lc, lw, _ = left
+    rc, rw, _ = right
+    if (rw > rc and rc != 0) or (lw > lc and lc != 0):
+        return False 
+    else:
+        return True 
+
 def expand(state):
-    print("incomplete: expand(state) :/")
-    return []
+    left, right = state
+    lc, lw, lb = left
+    rc, rw, rb = right
+    left_bank_animals = ["c" for _ in range(lc)] + ["w" for _ in range(lw)]
+    right_bank_animals = ["c" for _ in range(rc)] + ["w" for _ in range(rw)]
+    possible_states = set()
+    if lb: #boat is on left
+        animals_to_move = list(set(list(combinations(left_bank_animals, 2)) + left_bank_animals))
+        for b in animals_to_move:
+            new_left = (lc - b.count("c"), lw-b.count("w"), 0)
+            new_right = (rc+b.count("c"), rw+b.count("w"), 1)
+            possible_states.add((new_left, new_right))
+    else: #boat is on right
+        animals_to_move = list(set(list(combinations(right_bank_animals, 2)) + right_bank_animals))
+        for b in animals_to_move:
+            new_left = (lc + b.count("c"), lw+b.count("w"), 1)
+            new_right = (rc-b.count("c"), rw-b.count("w"), 0)
+            possible_states.add((new_left, new_right))
+            
+    for s in list(possible_states):
+        if state_is_legal(s):
+            yield s
+             
+
+
+def backtrace(back, start_from):
+    curr = start_from
+    while curr != -1:
+        yield curr
+        curr = back[curr]
+
+def graph_search(frontier, intial_state, goal_state):
+    explored = set()
+    cost = defaultdict(lambda: float('inf')) #use states as keys
+    back = defaultdict(lambda: -1) #use states as keys
+
+    cost[initial_state] = 0
+    frontier.put(initial_state) 
+    while(1):
+        if frontier.empty():
+            #solution not found
+            return -1, []
+         
+        leaf = frontier.get()
+        if(leaf == goal_state):
+            #return cost and path of states to get there
+            return cost[leaf], list(backtrace(back, leaf))
+ 
+        explored.add(leaf)
+        #need to define expand func: takes state, returns list of reachable states 
+        reachable = expand(leaf)
+        for s in list(reachable):
+            if cost[leaf] +1 < cost[s]:
+                cost[s] = cost[leaf] +1
+                back[s] = leaf
+            if s not in explored and s not in frontier.queue:
+                frontier.put(s)
+    
+
 
 args = get_args()
 
@@ -41,25 +109,9 @@ else:
     print("need to add frontier data structure for Astar :)")
     exit()
 
-#start gsearch psuedo code
-frontier.put(initial_state) 
-explored = set()
+cost, path = graph_search(frontier, initial_state, goal_state)
 
-while(1):
-    if frontier.empty():
-        #return failure
-        break
-    leaf = frontier.get()
-    if(leaf == goal_state):
-        #return solution
-        break
-    explored.add(leaf)
-    #need to define expand func: takes state, returns list of reachable states
-    reachable = expand(leaf)
-
-    for s in reachable:
-        if s not in explored and s not in frontier.queue:
-            frontier.put(s)
-    
-
-
+if cost == -1:
+    print("No solution found")
+else:
+    print("Cost:", cost, "Path:", path)
