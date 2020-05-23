ade6cf79d6ea02f4f2e9adbf96f017c5e845916f
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-05-23 16:19:52 -0700

    lol fixed np type struggles:

--- program3/binomial_bayes.py
+++ program3/binomial_bayes.py
@@ -4,21 +4,30 @@ import pandas as pd
 from string import punctuation
 
 #function takes in a space delimited string and returns a cleaned list of words
+#presently does not omit numbers (but it easily could)
 def clean_text(review):
     translator = str.maketrans('','',punctuation)
-    return str(review).translate(translator).lower().split()
+    return np.array(str(review).translate(translator).lower().split())
 
 def load_data():
     train = np.array(pd.read_csv("trainingSet.txt",sep="\t"))
     test = np.array(pd.read_csv("testSet.txt",sep="\t"))
     trainX, trainy = train.T
-    vocab = np.unique(np.array(clean_text(trainX)))
-    trainX = np.array([clean_text(x) for x in trainX])
-    testX = np.array([clean_text(x) for x in test])
+    vocab = np.unique(clean_text(trainX))
+    t = [clean_text(x) for x in trainX]
+    trainX = np.array(t)
+    t = [clean_text(x) for x in test]
+    testX = np.array(t)
     #trainX and testX are now a jagged array of cleaned examples
     return (trainX, trainy), testX, vocab
     
+# def bag_words(data, vocab):
+#     num_examples = data.shape[0]
+#     num_features = vocab.shape[0]
+#     matrix = np.zeros((num_examples, num_features))
+#     matrix[data in vocab] = 1
   
 train, test, vocabulary = load_data()
 trainX, trainY = train
+#bag_words(trainX, vocabulary)
 print()
